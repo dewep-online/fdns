3e560794d17d3a41fe704049e4fb9f2f433d20dc
Mikhail Knyazhev <markus621@gmail.com>
2020-03-27 00:15:17 +0300

    init

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 root=`dirname $0`
+mainfile=${root}/../cmd/fdns/main.go
 
-cd ${root}
+cd ${root}/bin
+rm -rf ./fdns*
 
-mkdir bin
-
-GO111MODULE=on GOOS=linux GOARCH=amd64 go build -o fdns_amd64 cmd/fdns/main.go
-#GO111MODULE=on GOOS=windows GOARCH=amd64 go build -o fdns_windows64.exe cmd/fdns/main.go
-#GO111MODULE=on GOOS=darwin GOARCH=amd64 go build -o fdns_macos cmd/fdns/main.go
+GO111MODULE=on GOOS=linux GOARCH=amd64 go build -o fdns_amd64 ${mainfile}
+GO111MODULE=on GOOS=windows GOARCH=amd64 go build -o fdns_windows64.exe ${mainfile}
+GO111MODULE=on GOOS=darwin GOARCH=amd64 go build -o fdns_macos ${mainfile}
